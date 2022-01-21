e7386654d9155357732148537a2c364a6efd6a8f
Milton Hernandez <52238198+miltonaxl@users.noreply.github.com>
2022-01-21 09:11:42 -0500

    change route api countries

--- src/config/URLS.js
+++ src/config/URLS.js
@@ -1,4 +1,4 @@
 export const URLS = {
-    rest:'https://restcountries.eu/rest/v2',
+    rest:'https://restcountries.com/v3.1',
     urlMap:'https://maps.googleapis.com/maps/api/js?v3.exp&key=AIzaSyCU1sg0H8sWuzA4IvM1NsO-erYuvsrELTM'
 }
