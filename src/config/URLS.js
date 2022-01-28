fa3bba64042a9c4b09d6cbe5d123f77fd5c33cb2
miltonaxl <miltonandresaxl@gmail.com>
2022-01-28 09:46:04 -0500

    countries app deployment aws

--- src/config/URLS.js
+++ src/config/URLS.js
@@ -1,5 +1,5 @@
 export const URLS = {
-  rest: "https://restcountries.eu/rest/v2",
+  rest: "https://restcountries.com/v2",
   urlMap:
     "https://maps.googleapis.com/maps/api/js?v3.exp&key=AIzaSyCU1sg0H8sWuzA4IvM1NsO-erYuvsrELTM",
 };
